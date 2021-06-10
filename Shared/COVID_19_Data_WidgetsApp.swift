c873f4c7cef6cb5329703dd021b3d89f20cb5f60
Alexander Lester <alexander480@gmail.com>
2021-06-09 23:05:26 -0400

    Initial Commit

---
+++ Shared/COVID_19_Data_WidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  COVID_19_Data_WidgetsApp.swift
+//  Shared
+//
+//  Created by Alexander Lester on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct COVID_19_Data_WidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
